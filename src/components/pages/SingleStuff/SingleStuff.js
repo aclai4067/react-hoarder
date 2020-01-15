b9f88f79850ea5cd748aaee52f804828865d4bc7
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-14 21:40:11 -0600

    delete works from single stuff page

--- src/components/pages/SingleStuff/SingleStuff.js
+++ src/components/pages/SingleStuff/SingleStuff.js
@@ -17,6 +17,14 @@ class SingleStuff extends React.Component {
       }).catch((err) => console.error('error from singleStuff', err));
   }
 
+  discardItemEvent = () => {
+    const { stuffId } = this.props.match.params;
+    stuffData.removeStuff(stuffId)
+      .then(() => {
+        this.props.history.push('/stuff');
+      }).catch((err) => console.error('error from single item delete', err));
+  }
+
   render() {
     const { item } = this.state;
     const { stuffId } = this.props.match.params;
@@ -26,7 +34,7 @@ class SingleStuff extends React.Component {
         <h1>{item.itemName}</h1>
         <div>
           <Link className='btn btn-light m-2' to={`/stuff/${stuffId}/edit`}>Edit</Link>
-          <button className='btn btn-dark m-2'>Delete</button>
+          <button className='btn btn-dark m-2' onClick={this.discardItemEvent}>Delete</button>
         </div>
         <img className='itemImg' src={item.itemImage} alt={item.itemName} />
         <p className='pt-4'>{item.itemDescription}</p>
