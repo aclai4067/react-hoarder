620c33ef2a1bff1017d65c269233591ab37c77f9
Ashley Claiborne <languageisaliquid@gmail.com>
2020-01-20 00:50:40 -0600

    styled home and updated readme

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import './Home.scss';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div className='Home'>
         <h1>Home</h1>
+        <Link className='btn btn-light stuffBtn' to='/stuff'>My Stuff</Link>
       </div>
     );
   }
